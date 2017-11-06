8d82c8e33827297c5f7ad9c9d03efed295369a6c
Mike Appleton <mikeapple@gmail.com>
2017-11-05 20:04:33 -0500

    Add files via upload

---
+++ DownloadFileWithProgress/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DownloadFileWithProgress
+{
+    class MainClass
+    {
+        private const string APIURL = "http://localhost:5000";
+        public static EventHandler<int> DownloadProgressUpdated;
+        private static int previousProgress;
+
+        public static void Main(string[] args)
+        {
+            DownloadProgressUpdated += (sender, e) => Console.WriteLine(e.ToString());
+            var cancellationToken = new CancellationTokenSource();
+
+            DownloadFileAsync("api/values/", cancellationToken.Token).Wait();
+        }
+
+        public static async Task<byte[]> DownloadFileAsync(string url, CancellationToken token)
+        {
+            previousProgress = 0;
+            var client = new HttpClient();
+            client.BaseAddress = new Uri(APIURL);
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await client.GetAsync(url, HttpCompletionOption.ResponseHeadersRead, token);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception(string.Format("The request returned with HTTP status code {0}", response.StatusCode));
+            }
+
+            var total = response.Content.Headers.ContentLength.HasValue ? response.Content.Headers.ContentLength.Value : -1L;
+            var canReportProgress = total != -1 && DownloadProgressUpdated != null;
+
+            using (var stream = await response.Content.ReadAsStreamAsync())
+            {
+                var totalRead = 0;
+                var buffer = new byte[total == -1 ? 4096 : total];
+                var isMoreToRead = true;
+
+                do
+                {
+                    token.ThrowIfCancellationRequested();
+
+                    var read = await stream.ReadAsync(buffer, totalRead, buffer.Length - totalRead, token);
+
+                    if (read == 0)
+                    {
+                        isMoreToRead = false;
+                    }
+                    else
+                    {
+                        totalRead += read;
+
+                        if (canReportProgress)
+                        {
+                            double progress = ((totalRead * 1d) / (total * 1d) * 100);
+                            UpdateDownloadProgress(progress);
+                        }
+                    }
+                } while (isMoreToRead);
+
+                UpdateDownloadProgress(100);
+
+                return buffer;
+            }
+        }
+
+        private static void UpdateDownloadProgress(double progress)
+        {
+            int currentProgress = (int)Math.Round(progress, 0);
+
+            if (currentProgress == previousProgress)
+                return;
+            
+            DownloadProgressUpdated?.Invoke(null, currentProgress);
+            previousProgress = currentProgress;
+        }
+    }
+}
---
+++ DownloadWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace DownloadWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            IEnumerable<string> randomText = Enumerable.Range(0, 999999).Select(s => s.ToString()).ToArray();
+            string serialised = JsonConvert.SerializeObject(randomText);
+            byte[] bytes = Encoding.UTF8.GetBytes(serialised);
+            Response.ContentLength = bytes.Length;
+
+            return randomText;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
